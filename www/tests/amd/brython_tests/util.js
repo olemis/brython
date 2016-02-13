a05985650bd27c28cf111578d896e3f038a0ebc7
Olemis Lang <olemis+work@gmail.com>
2016-02-12 23:58:41 -0500

    [amd] [parse] refs #188 - Wrap brython_tests.util AMD definition in function call

--- www/tests/amd/brython_tests/util.js
+++ www/tests/amd/brython_tests/util.js
@@ -1,22 +1,24 @@
 
-define({
-  'escape_string' : function(str) {
-    return ('' + str).replace(/["'\\\n\r\u2028\u2029]/g, function(ch) {
-      switch(ch) {
-        case '"' :
-        case '\\' :
-        case "'" :
-          return '\\' + ch;
-        case '\n' :
-          return '\\n';
-        case '\r' :
-          return '\\r';
-        case '\u2028' :
-          return '\\u2028';
-        case '\u2029' :
-          return '\\u2029';
-      }
-    })
-  },
-})
-
+;(function() {
+  define({
+    'escape_string' : function(str) {
+      return ('' + str).replace(/["'\\\n\r\u2028\u2029]/g, function(ch) {
+        switch(ch) {
+          case '"' :
+          case '\\' :
+          case "'" :
+            return '\\' + ch;
+          case '\n' :
+            return '\\n';
+          case '\r' :
+            return '\\r';
+          case '\u2028' :
+            return '\\u2028';
+          case '\u2029' :
+            return '\\u2029';
+        }
+      })
+    },
+    
+  })
+})()
