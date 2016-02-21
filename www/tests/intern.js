2c4a543c2eebbfb8e066d9fcda2ca8fa847f0734
Olemis Lang <olemis+work@gmail.com>
2016-02-21 00:41:33 -0500

    [amd] [tests] refs #188 - Refactor intern config code according to browser vs node.js

--- www/tests/intern.js
+++ www/tests/intern.js
@@ -13,34 +13,47 @@ client.html e.g. http://localhost/intern/client.html?amdLoader=http://localhost/
 */
 
 ;(function() {
-  // See stackoverflow:2976651
-  var stk = new Error().stack.split('\n');
-      idx = 0;
-  for (var i in stk) {
-    if (!stk[i].match(/http[s]?:\/\//)) continue;
-    idx = Number(i) + 1;
-    break;
-  }
-  var path_parts = stk[idx].match(/((http[s]?:\/\/.+\/)([^\/]+\.js)):/),
-      path_base = (path_parts? path_parts[2]: '') + '../',
-      path_requirejs_cdn = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.22/require.min.js',
-      amd_loader_arg = location.search.match(/[?&]amdLoader=([^&]+)/),
-      cfg_loader = {'host-node' : 'requirejs',
-                    'host-browser' : this['amd']? amd.loader_path :
-                                     (amd_loader_arg? amd_loader_arg[1] :
-                                                      path_requirejs_cdn)};
-      cfg_loader_options = {baseUrl: 'src/amd',
-                            paths: {'brython_tests' : '../../tests/amd/brython_tests'}}
 
-  define({
-    basePath: path_base,
-    suites: ['brython_tests/tkn'],
-    // Intern 2.0
-    useLoader: cfg_loader,
-    loader: cfg_loader_options,
-    // Intern 3.0
-    loaders: cfg_loader,
-    loaderOptions: cfg_loader_options
+  // FIXME : More reliable check for browser vs node.js
+  var is_browser = typeof window !== 'undefined';
+
+  function browser_params() {
+    // See stackoverflow:2976651
+    var stk = new Error().stack.split('\n');
+        idx = 0;
+    console.log(stk);
+    for (var i in stk) {
+      if (!stk[i].match(/http[s]?:\/\//)) continue;
+      idx = Number(i) + 1;
+      break;
+    }
+    var path_parts = stk[idx].match(/((http[s]?:\/\/.+\/)([^\/]+\.js)):/),
+        path_base = (path_parts? path_parts[2]: '') + '../',
+        path_requirejs_cdn = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.22/require.min.js',
+        amd_loader_arg = location.search.match(/[?&]amdLoader=([^&]+)/),
+        cfg_loader = {'host-node' : 'requirejs',
+                      'host-browser' : this['amd']? amd.loader_path :
+                                       (amd_loader_arg? amd_loader_arg[1] :
+                                                        path_requirejs_cdn)};
+        cfg_loader_options = {baseUrl: 'src/amd',
+                              paths: {'brython_tests' : '../../tests/amd/brython_tests'}}
+    return {basePath: path_base,
+            // Intern 2.0
+            useLoader: cfg_loader,
+            loader: cfg_loader_options,
+            // Intern 3.0
+            loaders: cfg_loader,
+            loaderOptions: cfg_loader_options}
+  }
+
+  function node_params() {
+    return {};
+  }
+
+  define(function() {
+    var params = (is_browser? browser_params: node_params)();
+    params.suites = ['brython_tests/tkn'];
+    return params;
   })
 })()
 
